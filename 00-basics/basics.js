3558a33370207ca365bb223be8080d9321fa3ca5
Sandro Micalizzi <hello@sandromicalizzi.com>
2017-04-24 14:26:09 +0200

    Ajout des descriptions des fonctions de la série BASICS

--- 00-basics/basics.js
+++ 00-basics/basics.js
@@ -1,56 +1,60 @@
 var Basics = function() {};
 
-// description
+// Calculer la somme de 2 nombres donnés
 Basics.prototype.somme2Nombres = function (nombre1, nombre2) {
   return (true);
 };
 
-// description
+// Calculer la moyenne de 2 nombres donnés
 Basics.prototype.moyenne2Nombres = function (nombre1, nombre2) {
   return (true);
 };
 
-// description
+// Calculer l'aire d'un triangle dont on connait la base et la hauteur
 Basics.prototype.aireTriangle = function (base, hauteur) {
   return (true);
 };
 
-// description
+// Calculer la circonférence d'un cercle dont on connait le rayon
 Basics.prototype.circonferenceCercle = function (rayon) {
   return (true);
 };
 
-// description
+// Calculer l'aire d'un cercle dont on connait le rayon
 Basics.prototype.aireCercle = function (rayon) {
   return (true);
 };
 
-// description
+// Calculer la TVA (20%) à partir du prix hors TVA d'un produit
 Basics.prototype.tva = function (prixHT) {
   return (true);
 };
 
-// description
-Basics.prototype.interets = function (investissement) {
+// Je place un montant (capital) à la banque pendant 1 an.
+// Calculer les intérêts que je toucherai. Le taux est de 2%.
+Basics.prototype.interets = function (capital) {
   return (true);
 };
 
-// description
-Basics.prototype.placement = function (investissement, tauxInteret) {
+// Je place un montant (capital) à la banque pendant 1 an.
+// Calculer le nouveau capital (capital de départ + intérêts).
+Basics.prototype.placement = function (capital, tauxInteret) {
   return (true);
 };
 
-// description
+// Calculer le montant total d'une facture dont on connait
+// le prix unitaire hors TVA, la quantité de produits et le taux de TVA.
 Basics.prototype.prixTTC = function (prixUnitaireHT, quantite, tauxTVA) {
   return (true);
 };
 
-// description
+// Calculer la durée (en secondes) d'un trajet dont on connait
+// la vitesse moyenne (en m/s == mètres par seconde) et la distance (en km).
 Basics.prototype.dureeTrajet = function (vitesseMoyenne, distance) {
   return (true);
 };
 
-// description
+// Calculer le nombre de secondes écoulées depuis minuit
 Basics.prototype.secondes = function (heure, minute, seconde) {
   return (true);
 };
