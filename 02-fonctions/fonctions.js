3e1fee60cf2f4aef1640e7d385800b6a95d3679d
Sandro Micalizzi <hello@sandromicalizzi.com>
2017-04-24 17:42:56 +0200

    Ajout de la série FONCTIONS

---
+++ 02-fonctions/fonctions.js
@@ -0,0 +1,35 @@
+var Basics = require("../00-basics/basics");
+var Conditions = require("../01-conditions/conditions");
+
+var Fonctions = function() {};
+
+// Recherche et retourne le plus grand nombre.
+// Utilise et ré-utilise la fonction maximum2Nombres() de la série "Conditions".
+Fonctions.prototype.maximum4nombres = function (nombre1, nombre2, nombre3, nombre4) {
+
+};
+
+// Calcule l'écart en secondes entre 2 durées.
+// Utilie et ré-utilie la fonction secondes() de la série "Basics".
+Fonctions.prototype.ecartDurees = function (heure1, minute1, seconde1,
+                                            heure2, minute2, seconde2) {
+
+};
+
+// Retourne vrai si l'année est bissextile, faux si elle ne l'est pas.
+// Une année est bissextile si :
+// - elle est multiple de 4;
+// - elle n'est pas multiple de 100;
+// - elle est multiple de 400;
+// 2012 et 2400 sont bissextiles. 2010 et 2100 ne le sont pas.
+Fonctions.prototype.estBissextile = function (annee) {
+
+};
+
+// Retourne le nombre de jours qu'il y a dans un mois donné.
+// Si c'est le mois de février, il retourner 28 ou 29 selon si l'année est bissextile.
+Fonctions.prototype.nombreJours = function (mois, annee) {
+
+};
+
+module.exports = Fonctions;
---
+++ 02-fonctions/fonctions.spec.js
@@ -0,0 +1,82 @@
+var Fonctions = require("./fonctions");
+var fonctions = new Fonctions();
+
+describe("Maximum 4 nombres", function () {
+  it("0, 1, 2, 3", function () {
+    expect(Fonctions.maximum4nombres(0, 1, 2, 3)).toEqual(3);
+  });
+
+  it("0, 1, 4, 3", function () {
+    expect(Fonctions.maximum4nombres(0, 1, 4, 3)).toEqual(4);
+  });
+
+  it("0, 5, 2, 3", function () {
+    expect(Fonctions.maximum4nombres(0, 1, 2, 3)).toEqual(5);
+  });
+
+  it("7, 1, 2, 3", function () {
+    expect(Fonctions.maximum4nombres(0, 1, 2, 3)).toEqual(7);
+  });
+
+  it("0, 1, 3, 3", function () {
+    expect(Fonctions.maximum4nombres(0, 1, 2, 3)).toEqual(3);
+  });
+
+  it("1, 3, 3, 1", function () {
+    expect(Fonctions.maximum4nombres(0, 1, 2, 3)).toEqual(3);
+  });
+});
+
+describe("Ecart de durées", function () {
+  it("23:42:21 - 13:21:42", function () {
+    expect(Fonctions.ecartDurees(23, 42, 21, 13, 21, 42)).toEqual(37239);
+  });
+
+  it("12:12:12 - 22:22:22", function () {
+    expect(Fonctions.ecartDurees(12, 12, 12, 22, 22, 22)).toEqual(36610);
+  });
+});
+
+describe("Année bissextile", function () {
+  it("2010", function () {
+    expect(Fonctions.estBissextile(2010)).toBe(false);
+  });
+
+  it("2012", function () {
+    expect(Fonctions.estBissextile(2012)).toBe(true);
+  });
+
+  it("2100", function () {
+    expect(Fonctions.estBissextile(2100)).toBe(false);
+  });
+
+  it("2400", function () {
+    expect(Fonctions.estBissextile(2400)).toBe(true);
+  });
+
+  it("1980", function () {
+    expect(Fonctions.estBissextile(1980)).toBe(true);
+  });
+
+  it("1985", function () {
+    expect(Fonctions.estBissextile(1985)).toBe(false);
+  });
+});
+
+describe("Nombre de jours dans un mois", function () {
+  it("2, 2010", function () {
+    expect(Fonctions.nombreJours(2, 2010)).toEqual(28);
+  });
+
+  it("2, 2012", function () {
+    expect(Fonctions.nombreJours(2, 2012)).toEqual(29);
+  });
+
+  it("5, 2013", function () {
+    expect(Fonctions.nombreJours(5, 2013)).toEqual(31);
+  });
+
+  it("6, 1985", function () {
+    expect(Fonctions.nombreJours(6, 1985)).toEqual(30);
+  });
+});
