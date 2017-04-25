64f57d2ea122389b0949548fc6ae5822aece3b58
Sandro Micalizzi <hello@sandromicalizzi.com>
2017-04-25 12:19:14 +0200

    Rename Basics -> Bases

--- 00-bases/bases.js
+++ 00-bases/bases.js
@@ -1,62 +1,62 @@
-var Basics = function() {};
+var Bases = function() {};
 
 // Calculer la somme de 2 nombres donnés
-Basics.prototype.somme2Nombres = function (nombre1, nombre2) {
+Bases.prototype.somme2Nombres = function (nombre1, nombre2) {
   return (true);
 };
 
 // Calculer la moyenne de 2 nombres donnés
-Basics.prototype.moyenne2Nombres = function (nombre1, nombre2) {
+Bases.prototype.moyenne2Nombres = function (nombre1, nombre2) {
   return (true);
 };
 
 // Calculer l'aire d'un triangle dont on connait la base et la hauteur
-Basics.prototype.aireTriangle = function (base, hauteur) {
+Bases.prototype.aireTriangle = function (base, hauteur) {
   return (true);
 };
 
 // Calculer la circonférence d'un cercle dont on connait le rayon
-Basics.prototype.circonferenceCercle = function (rayon) {
+Bases.prototype.circonferenceCercle = function (rayon) {
   return (true);
 };
 
 // Calculer l'aire d'un cercle dont on connait le rayon
-Basics.prototype.aireCercle = function (rayon) {
+Bases.prototype.aireCercle = function (rayon) {
   return (true);
 };
 
 // Calculer la TVA (20%) à partir du prix hors TVA d'un produit
-Basics.prototype.tva = function (prixHT) {
+Bases.prototype.tva = function (prixHT) {
   return (true);
 };
 
 // Je place un montant (capital) à la banque pendant 1 an.
 // Calculer les intérêts que je toucherai. Le taux est de 2%.
-Basics.prototype.interets = function (capital) {
+Bases.prototype.interets = function (capital) {
   return (true);
 };
 
 // Je place un montant (capital) à la banque pendant 1 an.
 // Calculer le nouveau capital (capital de départ + intérêts).
-Basics.prototype.placement = function (capital, tauxInteret) {
+Bases.prototype.placement = function (capital, tauxInteret) {
   return (true);
 };
 
 // Calculer le montant total d'une facture dont on connait
 // le prix unitaire hors TVA, la quantité de produits et le taux de TVA.
-Basics.prototype.prixTTC = function (prixUnitaireHT, quantite, tauxTVA) {
+Bases.prototype.prixTTC = function (prixUnitaireHT, quantite, tauxTVA) {
   return (true);
 };
 
 // Calculer la durée (en secondes) d'un trajet dont on connait
 // la vitesse moyenne (en m/s == mètres par seconde) et la distance (en km).
-Basics.prototype.dureeTrajet = function (vitesseMoyenne, distance) {
+Bases.prototype.dureeTrajet = function (vitesseMoyenne, distance) {
   return (true);
 };
 
 // Calculer le nombre de secondes écoulées depuis minuit
-Basics.prototype.secondes = function (heure, minute, seconde) {
+Bases.prototype.secondes = function (heure, minute, seconde) {
   return (true);
 };
 
-module.exports = Basics;
+module.exports = Bases;
--- 00-bases/bases.spec.js
+++ 00-bases/bases.spec.js
@@ -1,162 +1,162 @@
-var Basics = require("./basics");
-var basics = new Basics();
+var Bases = require("./bases");
+var bases = new Bases();
 
 describe("Somme de 2 nombres", function () {
   it("1 + 2", function () {
-    expect(basics.somme2Nombres(1, 2)).toEqual(3);
+    expect(bases.somme2Nombres(1, 2)).toEqual(3);
   });
 
   it("(-1) + 2", function () {
-    expect(basics.somme2Nombres(-1, 2)).toEqual(1);
+    expect(bases.somme2Nombres(-1, 2)).toEqual(1);
   });
 
   it("1 + (-2)", function () {
-    expect(basics.somme2Nombres(1, -2)).toEqual(-1);
+    expect(bases.somme2Nombres(1, -2)).toEqual(-1);
   });
 
   it("(-1) + (-2)", function () {
-    expect(basics.somme2Nombres(-1, -2)).toEqual(-3);
+    expect(bases.somme2Nombres(-1, -2)).toEqual(-3);
   });
 
   it("0 + 0", function () {
-    expect(basics.somme2Nombres(0, 0)).toEqual(0);
+    expect(bases.somme2Nombres(0, 0)).toEqual(0);
   });
 });
 
 describe("Moyenne de 2 nombres", function () {
   it("Moyenne de 1 et 1", function () {
-    expect(basics.moyenne2Nombres(1, 1)).toEqual(1);
+    expect(bases.moyenne2Nombres(1, 1)).toEqual(1);
   });
 
   it("Moyenne de 1 et 2", function () {
-    expect(basics.moyenne2Nombres(1, 2)).toEqual(1.5);
+    expect(bases.moyenne2Nombres(1, 2)).toEqual(1.5);
   });
 
   it("Moyenne de (-1) et 1", function () {
-    expect(basics.moyenne2Nombres(-1, 1)).toEqual(0);
+    expect(bases.moyenne2Nombres(-1, 1)).toEqual(0);
   });
 });
 
 
 describe("Aire d'un triangle", function () {
   it("Base = 1 | Hauteur = 2", function () {
-    expect(basics.aireTriangle(1, 2)).toEqual(1);
+    expect(bases.aireTriangle(1, 2)).toEqual(1);
   });
 
   it("Base = 3 | Hauteur = 5", function () {
-    expect(basics.aireTriangle(3, 5)).toEqual(7.5);
+    expect(bases.aireTriangle(3, 5)).toEqual(7.5);
   });
 });
 
 describe("Circonférence d'un cercle", function () {
   it("Rayon = 1", function () {
-    expect(basics.circonferenceCercle(1)).toEqual(6.283185307179586);
+    expect(bases.circonferenceCercle(1)).toEqual(6.283185307179586);
   });
 
   it("Rayon = 3", function () {
-    expect(basics.circonferenceCercle(3)).toEqual(18.84955592153876);
+    expect(bases.circonferenceCercle(3)).toEqual(18.84955592153876);
   });
 });
 
 
 describe("Aire d'un cercle", function () {
   it("Rayon = 2", function () {
-    expect(basics.aireCercle(2)).toEqual(12.566370614359172);
+    expect(bases.aireCercle(2)).toEqual(12.566370614359172);
   });
 
   it("Rayon = 3", function () {
-    expect(basics.aireCercle(3)).toEqual(28.274333882308138);
+    expect(bases.aireCercle(3)).toEqual(28.274333882308138);
   });
 });
 
 describe("TVA", function () {
   it("Prix HT = 100 euros", function () {
-    expect(basics.tva(100)).toEqual(20);
+    expect(bases.tva(100)).toEqual(20);
   });
 
   it("Prix HT = 199 euros", function () {
-    expect(basics.tva(199)).toEqual(39.800000000000004);
+    expect(bases.tva(199)).toEqual(39.800000000000004);
   });
 
   it("Prix HT = 1.99 euros", function () {
-    expect(basics.tva(1.99)).toEqual(0.398);
+    expect(bases.tva(1.99)).toEqual(0.398);
   });
 });
 
 describe("Intérêts", function () {
   it("Pour 100 euros", function () {
-    expect(basics.interets(100)).toEqual(2);
+    expect(bases.interets(100)).toEqual(2);
   });
 
   it("Pour 199 euros", function () {
-    expect(basics.interets(199)).toEqual(3.98);
+    expect(bases.interets(199)).toEqual(3.98);
   });
 
   it("Pour 1.99 euros", function () {
-    expect(basics.interets(1.99)).toEqual(0.0398);
+    expect(bases.interets(1.99)).toEqual(0.0398);
   });
 });
 
 describe("Nouvelle valeur du capital placé", function () {
   it("Je place 100 euros à 3%", function () {
-    expect(basics.placement(100, 3)).toEqual(103);
+    expect(bases.placement(100, 3)).toEqual(103);
   });
 
   it("Je place 199 euros à 5%", function () {
-    expect(basics.placement(199, 5)).toEqual(208.95000000000002);
+    expect(bases.placement(199, 5)).toEqual(208.95000000000002);
   });
 
   it("Je place 1.99 euros à 1.5%", function () {
-    expect(basics.placement(1.99, 1.5)).toEqual(2.01985);
+    expect(bases.placement(1.99, 1.5)).toEqual(2.01985);
   });
 });
 
 describe("Montant total de la facture TTC", function () {
   it("100 euros HT * 2 unités * TVA 20%", function () {
-    expect(basics.prixTTC(100, 2, 20)).toEqual(240);
+    expect(bases.prixTTC(100, 2, 20)).toEqual(240);
   });
 
   it("199 euros HT * 9 unités * TVA 20%", function () {
-    expect(basics.prixTTC(199, 9, 20)).toEqual(2149.2);
+    expect(bases.prixTTC(199, 9, 20)).toEqual(2149.2);
   });
 
   it("1.99 euros HT * 42 unités * TVA 20%", function () {
-    expect(basics.prixTTC(1.99, 42, 20)).toEqual(100.29599999999999);
+    expect(bases.prixTTC(1.99, 42, 20)).toEqual(100.29599999999999);
   });
 });
 
 describe("Durée du trajet", function () {
   it("Vitesse moyenne = 4 m/s | Distance = 1 km", function () {
-    expect(basics.dureeTrajet(4, 1)).toEqual(250);
+    expect(bases.dureeTrajet(4, 1)).toEqual(250);
   });
 
   it("Vitesse moyenne = 16 m/s | Distance = 4.2 km", function () {
-    expect(basics.dureeTrajet(16, 4.2)).toEqual(262.5);
+    expect(bases.dureeTrajet(16, 4.2)).toEqual(262.5);
   });
 
   it("Vitesse moyenne = 123 m/s | Distance = 42 km", function () {
-    expect(basics.dureeTrajet(123, 42)).toEqual(341.4634146341463);
+    expect(bases.dureeTrajet(123, 42)).toEqual(341.4634146341463);
   });
 });
 
 describe("Secondes passées depuis minuit", function () {
   it("1 heure, 1 minute et 1 seconde", function () {
-    expect(basics.secondes(1, 1, 1)).toEqual(3661);
+    expect(bases.secondes(1, 1, 1)).toEqual(3661);
   });
 
   it("13 heures, 42 minutes et 42 secondes", function () {
-    expect(basics.secondes(13, 42, 42)).toEqual(49362);
+    expect(bases.secondes(13, 42, 42)).toEqual(49362);
   });
 
   it("1 heure, 1 minute et 0 seconde", function () {
-    expect(basics.secondes(1, 1, 0)).toEqual(3660);
+    expect(bases.secondes(1, 1, 0)).toEqual(3660);
   });
 
   it("1 heure, 0 minute et 1 seconde", function () {
-    expect(basics.secondes(1, 0, 1)).toEqual(3601);
+    expect(bases.secondes(1, 0, 1)).toEqual(3601);
   });
 
   it("0 heure, 1 minute et 1 seconde", function () {
-    expect(basics.secondes(0, 1, 1)).toEqual(61);
+    expect(bases.secondes(0, 1, 1)).toEqual(61);
   });
 });
